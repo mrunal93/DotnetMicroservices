ab941edf24108cabc053b888486ad0084c2cad7c
Mrunal fulzele <mrn26893@gmail.com>
2025-09-01 19:31:58 +0530

    Coupon services Interface to consume the API for coupon

---
+++ Mango/Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+
+        Task<ResponseDto?> GetCouponasync(string couponId);
+        Task<ResponseDto?> GetAllCouponsasync();
+        Task<ResponseDto?> GetCouponBYIDasync(int ID);
+        Task<ResponseDto?> CreateCouponasync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponasync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponasync(int ID);
+    }
+}
--- Mango/Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango/Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5022cd2fb6555cb680dac826410ad6edac36940b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b463cd28b0f9f6fc18bcff95356ca37f8d97d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
